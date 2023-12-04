f68177e8bdf1d7c4cda3ac02fa8d676168d69cd6
verandrespalacio <andrespalaciovera@gmail.com>
2023-12-04 08:49:06 -0500

    New branch with different design and components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './platzi.webp';
-import './App.css';
+import React from 'react';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
+import { TodoBtn } from './TodoBtn';
+
+let defaultTodos = [
+  {text: 'To laught till cry', completed: true},
+  {text: 'To sing till to be hoarse', completed: false},
+  {text: 'To pray till to be calm', completed: true},
+  {text: 'To sleep till to be ready', completed: false},
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TodoCounter total={20} completed={15}/>
+      <TodoSearch/>
+
+      <TodoList>
+        {defaultTodos.map(todo=> (
+          <TodoItem key={todo.text} text={todo.text} completed={todo.completed}/>
+        ))}
+      </TodoList>
+
+      <TodoBtn/>
+    </>
   );
 }
 
